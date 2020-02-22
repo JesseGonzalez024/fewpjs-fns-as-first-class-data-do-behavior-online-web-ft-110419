2e8f6233ace77fbff548df53a99199ce879f6758
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-02-22 01:37:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ function greet(string) {
   let time = string.split(":")
   
   if (parseInt(time[0]) < 12) {
-    console.log("Good Morning")
+    return "Good Morning"
+    
   } else if (parseInt(time[0]) >= 12 && parseInt(time[0]) <= 17) {
-    console.log("Good Afternoon")
+    
+    return "Good Afternoon"
   } else {
-     console.log("Good Evening") 
+     return "Good Evening"
   }
 }
 
